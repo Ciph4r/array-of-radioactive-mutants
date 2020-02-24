6ec1a64061656f11f9222130f708bba2bb28201d
David Lau <David.lau@codeimmersives.com>
2020-02-24 13:51:14 -0500

    finished

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+addMeToEnd = (x) => {
+  return x.push('Colin')
+}
+
+addMeToStart = (x) => {
+  return x.unshift('Colin')
+}
 
 
+changeLast = (x,y) => {
+  return x[x.length-1] = y
+}
+
+changeAllValuesTo = (x,y) => {
+  for (let i = 0 ; x.length > i ; i++){
+    x[i] = y
+  }
+  return x
+}
+changeAllValuesTo([1,2,4,8], 9)
 
 
+oddOrEven = (x) => {
+  for (let i = 0 ; x.length > i ; i++)
+  x[i] %2 === 0 ? x[i] = 'even' : x[i] = 'odd'
+
+}
+
+changeNextThreeToValue=(s,a,v) => {
+  for (let i = 0  ; a.length > i ; i++)
+      if (i >= s && i < s+3)  { 
+        a[i] = v
+      }
+  
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
